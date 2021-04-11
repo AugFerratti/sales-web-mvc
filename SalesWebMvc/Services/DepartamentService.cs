fb73c92158a096994fab9a161cfc99b76dd400ee
AugFerratti <augusto.ferratti@gmail.com>
2021-04-11 12:43:22 -0300

    SellerFormViewModel e componente select para departamento

--- SalesWebMvc/Services/DepartamentService.cs
+++ SalesWebMvc/Services/DepartamentService.cs
@@ -5,26 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace SalesWebMvc.Services
 {
-    public class SellerService
+    public class DepartamentService
     {
         private readonly SalesWebMvcContext _context;
 
-        public SellerService(SalesWebMvcContext context)
+        public DepartamentService(SalesWebMvcContext context)
         {
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public List<Departament> FindAll()
         {
-            return _context.Seller.ToList();
-        }
-
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
+            return _context.Departament.OrderBy(x => x.Name).ToList();
         }
     }
 }
